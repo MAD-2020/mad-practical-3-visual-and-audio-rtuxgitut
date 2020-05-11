d1afd30d5a4a7e0ed25d7351539ea904ba416ea8
Wei Hao Lim <s10185304@connect.np.edu.sg>
2020-05-11 14:15:35 +0800

    Week 3 Exercise done!

--- app/src/main/java/sg/edu/np/mad/week3/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/week3/MainActivity.java
@@ -3,6 +3,8 @@ package sg.edu.np.mad.week3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.net.Uri;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        VideoView video = findViewById(R.id.video);
+        video.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.rabbid));
+        video.start();
+
     }
 }
